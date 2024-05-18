5d204f2d6a6d0cfdde3c10658bd74f851124503c
Jakub Spólnik <85324038+lawos98@users.noreply.github.com>
2024-05-18 18:41:38 +0200

    GEM-70 | Add Validators part (#9)
    * GEM-70 | Add Validators part
    
    * GEM-70 | Update version

---
+++ src/main/kotlin/pl/edu/agh/gem/validator/BaseValidator.kt
@@ -0,0 +1,9 @@
+package pl.edu.agh.gem.validator
+
+abstract class BaseValidator<T : DataWrapper> {
+    protected abstract val checks: List<Check<T>>
+
+    fun apply(dataWrapper: T) {
+        checks.map { it.apply(dataWrapper) }
+    }
+}
