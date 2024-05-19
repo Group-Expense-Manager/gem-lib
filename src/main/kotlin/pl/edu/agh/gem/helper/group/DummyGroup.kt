71fb8534e36bb5202f3e960b49735aacc736c220
Paweł Gacek <pawelgacek.pl@gmail.com>
2024-05-19 13:49:21 +0200

    GEM-81 | fix

--- src/main/kotlin/pl/edu/agh/gem/helper/group/DummyGroup.kt
+++ src/main/kotlin/pl/edu/agh/gem/helper/group/DummyGroup.kt
@@ -2,7 +2,6 @@ package pl.edu.agh.gem.helper.group
 
 import pl.edu.agh.gem.dto.GroupMemberResponse
 import pl.edu.agh.gem.dto.GroupMembersResponse
-import pl.edu.agh.gem.helper.group.DummyGroup.OTHER_GROUP_ID
 import pl.edu.agh.gem.helper.user.DummyUser.USER_ID
 import pl.edu.agh.gem.model.GroupMember
 import pl.edu.agh.gem.model.GroupMembers
@@ -11,16 +10,6 @@ object DummyGroup {
     const val GROUP_ID = "groupId"
     const val OTHER_GROUP_ID = "otherGroupId"
 }
-
-fun createGroupMembersResponse(
-    user: String = USER_ID,
-) = GroupMembersResponse(
-    members = listOf(
-        createGroupMemberResponse(
-            id = user,
-        ),
-    ),
-)
 
 fun createGroupMembersResponse(
     vararg users: String = arrayOf(USER_ID),
@@ -38,12 +27,6 @@ fun createGroupMembers(
     vararg users: String = arrayOf(USER_ID),
 ) = GroupMembers(
     members = users.map { GroupMember(it) },
-)
-
-fun createGroupMembers(
-    users: List<String> = listOf(USER_ID, OTHER_GROUP_ID),
-) = GroupMembers(
-    members = users.map { createGroupMember(it) },
 )
 
 fun createGroupMember(
