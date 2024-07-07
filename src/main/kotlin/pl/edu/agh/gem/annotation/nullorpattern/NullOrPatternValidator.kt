76014b4c8df6d58fe77a1e553f75e3b3bfe52cad
Paweł Gacek <pawelgacek.pl@gmail.com>
2024-07-07 14:46:02 +0200

    GEM-40 | add NullOrPattern annotation

---
+++ src/main/kotlin/pl/edu/agh/gem/annotation/nullorpattern/NullOrPatternValidator.kt
@@ -0,0 +1,16 @@
+package pl.edu.agh.gem.annotation.nullorpattern
+
+import jakarta.validation.ConstraintValidator
+import jakarta.validation.ConstraintValidatorContext
+import java.util.regex.Pattern
+
+class NullOrPatternValidator : ConstraintValidator<NullOrPattern, String> {
+    private lateinit var pattern: Pattern
+
+    override fun initialize(constraintValidator: NullOrPattern) {
+        pattern = Pattern.compile(constraintValidator.pattern)
+    }
+    override fun isValid(value: String?, constraintValidatorContext: ConstraintValidatorContext): Boolean {
+        return value == null || pattern.matcher(value).matches()
+    }
+}
