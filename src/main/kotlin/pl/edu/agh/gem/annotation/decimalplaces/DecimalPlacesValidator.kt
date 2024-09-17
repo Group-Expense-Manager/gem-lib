b86980d8b28c2dcee80a8df0b54ff10e81012ca9
Paweł Gacek <pawelgacek.pl@gmail.com>
2024-09-17 17:50:58 +0200

    GEM-209 | add DecimalPlacesValidator

---
+++ src/main/kotlin/pl/edu/agh/gem/annotation/decimalplaces/DecimalPlacesValidator.kt
@@ -0,0 +1,18 @@
+package pl.edu.agh.gem.annotation.decimalplaces
+
+import jakarta.validation.ConstraintValidator
+import jakarta.validation.ConstraintValidatorContext
+import java.math.BigDecimal
+import kotlin.properties.Delegates
+
+class DecimalPlacesValidator : ConstraintValidator<DecimalPlaces, BigDecimal> {
+    private var maxDecimalPlaces by Delegates.notNull<Int>()
+
+    override fun initialize(constraintAnnotation: DecimalPlaces) {
+        this.maxDecimalPlaces = constraintAnnotation.max
+    }
+
+    override fun isValid(value: BigDecimal, context: ConstraintValidatorContext): Boolean {
+        return value.scale() <= maxDecimalPlaces
+    }
+}
